3a3ac6c91967c5f923537d9bdc21a45f747a516d
samanthastaheli <sta180011@byui.edu>
2020-05-16 19:07:06 -0700

    wow website

---
+++ lesson04/js/preston.js
@@ -0,0 +1,17 @@
+//menu
+
+function toggleMenu() {
+    var hambutton = document.querySelector('.ham');
+    var mainnav = document.querySelector('.navigation')
+    document.getElementsByClassName('navigation')[0].classList.toggle('responsive');
+}
+
+//current date
+var wnames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var mnames = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var mydate = new Date();
+var week = wnames[mydate.getDay()];
+var month = mnames[mydate.getMonth()];
+var output = week + ", " + mydate.getDate() + " " + month + " " + mydate.getFullYear();
+
+document.getElementById('currentdate').textContent = output;
