c51d51dd82be0b76b6fa06541cccc1d704089974
samanthastaheli <sta180011@byui.edu>
2020-05-21 14:23:26 -0700

    weath sum and forcasst

---
+++ lesson05/js/preston.js
@@ -0,0 +1,23 @@
+//menu
+
+function toggleMenu() {
+    var hambutton = document.querySelector('.ham');
+    var mainnav = document.querySelector('.navigation')
+    document.getElementsByClassName('navigation')[0].classList.toggle('responsive');
+}
+
+//current date
+var wnames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var mnames = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var mydate = new Date();
+var week = wnames[mydate.getDay()];
+var month = mnames[mydate.getMonth()];
+var output = week + ", " + mydate.getDate() + " " + month + " " + mydate.getFullYear();
+
+
+if (week = 'Thursday') {
+    document.getElementById("friday").style.display = "block";
+}
+
+
+document.getElementById('currentdate').textContent = output;
