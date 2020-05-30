d5cca2ef15db766ed3929f3b421d8de05006ec2f
samanthastaheli <sta180011@byui.edu>
2020-05-30 14:30:16 -0700

    wind chill

--- lesson06/js/preston.js
+++ lesson06/js/preston.js
@@ -20,4 +20,11 @@ if (week = 'Sunday') {
     document.getElementById("friday").style.display = "block";
 }
 
+// wind chill
+wind= parseInt(document.getElementById('wind').textContent);
+temp= parseInt(document.getElementById('temp').textContent);
 
+chill=Math.round((0.0817*(3.71*(Math.pow(wind, 0.5))+
+5.81-0.25*wind)*(temp-91.4)+91.4));
+
+document.getElementById('chill').textContent = chill;
