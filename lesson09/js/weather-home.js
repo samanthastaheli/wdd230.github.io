ed1e90d5110d66f02d3eac0eae9a3fe79d0f0d33
samanthastaheli <sta180011@byui.edu>
2020-06-22 20:20:05 -0700

    home css

--- lesson09/js/weather-home.js
+++ lesson09/js/weather-home.js
@@ -6,31 +6,45 @@ fetch(requestURL)
     })
     .then(function (jsonObject) {
         console.table(jsonObject);
-        const towns = jsonObject['towns'];
+        const source = jsonObject['towns'];
+        let towns = []
+        j=0
+
+        for (let i = 0; i < source.length; i++) {
+            if (source[i].name == "Preston" || source[i].name == "Fish Haven" || source[i].name == "Soda Springs") {
+                towns[j] = source[i]
+                j++
+            }
+        }
 
         for (let i = 0; i < towns.length; i++ ) {
             let card = document.createElement('section');
+            let div = document.createElement('div');
             let h2 = document.createElement('h2');
             let h3 = document.createElement('h3');
             let p1 = document.createElement('p');
             let p2 = document.createElement('p');
             let p3 = document.createElement('p');
-            let image = document.createElement('img');
+            let photo = document.createElement('img');
         
+            div.classList.add('textDiv'); 
+            photo.setAttribute('src', 'img/' + towns[i].photo);
             h2.textContent = towns[i].name;
             h3.textContent = towns[i].motto;
-            p1.textContent = 'Year Founded: ' + towns[i].yearfounded;
+            p1.textContent = 'Year Founded: ' + towns[i].yearFounded;
             p2.textContent = 'Population: ' + towns[i].currentPopulation;
-            p2.textContent = 'Annual Rain Fall: ' + towns[i].averageRainfall;
-            image.setAttribute('src', towns[i].photo);
+            p3.textContent = 'Annual Rain Fall: ' + towns[i].averageRainfall;
+            
 
-            card.appendChild(h2);
-            card.appendChild(h3);
-            card.appendChild(p1);
-            card.appendChild(p2);
-            card.appendChild(p3);
-            card.appendChild(image);
-            document.querySelector('.cards').appendChild(card);
+            card.appendChild(photo);
+            card.appendChild(div);
+            div.appendChild(h2);
+            div.appendChild(h3);
+            div.appendChild(p1);
+            div.appendChild(p2);
+            div.appendChild(p3);
+           
+            document.querySelector('div.cards').appendChild(card);
         }
     })
     .catch(function (error){
